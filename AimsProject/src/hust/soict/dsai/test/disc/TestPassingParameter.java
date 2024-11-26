163479c374ed0e218029bd3c1b99762ed58467b3
Lang Tien <langtienk4@gmail.com>
2024-11-27 02:14:35 +0700

    Passing parameter

--- AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
+++ AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -1,5 +1,36 @@
 package hust.soict.dsai.test.disc;
 
-public class TestPassingParameter {
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        System.out.println("Before swap:");
+        System.out.println("jungleDVD title: " + jungleDVD.getTitle());
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
+
+        swap(jungleDVD, cinderellaDVD);
+
+        System.out.println("After swap:");
+        System.out.println("jungleDVD title: " + jungleDVD.getTitle());
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+
+        System.out.println("After changeTitle:");
+        System.out.println("jungleDVD title: " + jungleDVD.getTitle());
+        System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
+    }
+
+    public static void swap(DigitalVideoDisc disc1, DigitalVideoDisc disc2) {
+        String tempTitle = disc1.getTitle();
+        disc1.setTitle(disc2.getTitle());
+        disc2.setTitle(tempTitle);
+    }
+
+    public static void changeTitle(DigitalVideoDisc disc, String title) {
+        disc.setTitle(title);
+    }
 }
